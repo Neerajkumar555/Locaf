defe74cf647d8dd942de33836d8f01231a668cb6
Neeraj Kumar <b.neerajkumar9666@gmail.com>
2021-03-10 18:38:06 +0530

    login page changes

---
+++ scripts/firabase_login.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyD8oTTE14NpUqQUBhk_4iXYxlG5o8UBvuk",
+    authDomain: "locaf-f1e23.firebaseapp.com",
+    projectId: "locaf-f1e23",
+    storageBucket: "locaf-f1e23.appspot.com",
+    messagingSenderId: "485384071499",
+    appId: "1:485384071499:web:bdd7eded39c2443272a0ce"
+  };
+        // Initialize Firebase
+        firebase.initializeApp(firebaseConfig);
+        const db = firebase.firestore();
