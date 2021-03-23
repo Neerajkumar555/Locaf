ba90857e60a1b02d77e766ab0e93688ec4441405
Richard Mac <rmac2@my.bcit.ca>
2021-03-23 13:39:19 -0700

    profile js changes

--- scripts/profile.js
+++ scripts/profile.js
@@ -1,17 +1,17 @@
-function sayHello() {
+function getUsername() {
     firebase.auth().onAuthStateChanged(function (users) {
         if (users) {
             // User is signed in.
-            // Do something for the user here. 
+            
             console.log(users.uid);
             db.collection("users").doc(users.uid)
                 .get()
                 .then(function (doc) {
                     var n = doc.data().name;
                     console.log(n);
-										//using jquery
+										
                     $("#username").text(n);
-										//using vanilla javascript
+										
                     //document.getElementById("username").innerText = n;
                 })
         } else {
@@ -19,6 +19,6 @@ function sayHello() {
         }
     });
 }
-sayHello();
+getUsername();
 
 
