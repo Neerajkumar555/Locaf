a9f74173a2953dbeb3f2216a40c38dcfda640188
Parsa Boojari <pboojari@my.bcit.ca>
2021-03-24 10:41:25 -0700

    Using firebase in the Search page

--- scripts/submitlocation.js
+++ scripts/submitlocation.js
@@ -40,4 +40,20 @@ function submitLocation() {
         }, 1000);
     })
 }
-submitLocation();
+submitMyLocation();
+function mylocation(){
+    firebase.auth().onAuthStateChanged(function(locations){
+        if(locations){
+           console.log(locations.uid);
+           db.collection("locations")
+           .doc(locations.uid)
+           .get()
+           .then(function(doc){
+               console.log(doc.data().address);
+               var n = doc.data().adress;
+               $("#my-address").text(n);
+           })
+        }
+    })
+}
+submitMyLocation(); 
