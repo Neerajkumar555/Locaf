280b850ebb93b92521d6157099022f633580033c
Oscar La <oscarla5747@gmail.com>
2021-03-24 11:14:51 -0700

    display locations

--- scripts/search.js
+++ scripts/search.js
@@ -1,22 +1,22 @@
-
-// submitMyLocation();
-// function mylocation(){
-//     firebase.auth().onAuthStateChanged(function(locations){
-//         if(locations){
-//            console.log(locations.uid);
-//            db.collection("locations")
-//            .doc(locations.uid)
-//            .get()
-//            .then(function(doc){
-//                console.log(doc.data().address);
-//                var n = doc.data().adress;
-//                $("#my-address").text(n);
-//            })
-//         }
-//     })
-// }
-// submitMyLocation(); 
-
+//
+//submitMyLocation();
+//function mylocation(){
+//    firebase.auth().onAuthStateChanged(function(locations){
+//        if(locations){
+//           console.log(locations.uid);
+//           db.collection("locations")
+//           .doc(locations.uid)
+//           .get()
+//           .then(function(doc){
+//               console.log(doc.data().address);
+//               var n = doc.data().adress;
+//               $("#my-address").text(n);
+//           })
+//        }
+//    })
+//}
+//submitMyLocation(); 
+//
 // try this ..
 
 function getlocation(){
@@ -25,9 +25,11 @@ function getlocation(){
     .then(function(snap){
         snap.forEach(function(doc){
             var n = doc.data().name;
-            var add = doc.data().adress;
+            var add = doc.data().address;
             console.log(n);
-            $("#my-address").text(add);
+            var text = document.createElement("p");
+            text.innerHTML = (n + " " + add);
+            $("#my-address").append(text);
         })
     })
 }
