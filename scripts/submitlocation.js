fdbe05369675c4c7d4225dfd46bc5cf349e697fb
Oscar La <oscarla5747@gmail.com>
2021-03-24 10:48:30 -0700

    deleted extra code

--- scripts/submitlocation.js
+++ scripts/submitlocation.js
@@ -41,19 +41,3 @@ function submitLocation() {
     })
 }
 submitMyLocation();
-function mylocation(){
-    firebase.auth().onAuthStateChanged(function(locations){
-        if(locations){
-           console.log(locations.uid);
-           db.collection("locations")
-           .doc(locations.uid)
-           .get()
-           .then(function(doc){
-               console.log(doc.data().address);
-               var n = doc.data().adress;
-               $("#my-address").text(n);
-           })
-        }
-    })
-}
-submitMyLocation(); 
