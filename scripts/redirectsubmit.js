50781f11eee34bb8f814b8b55cbaa1d72445b046
Neeraj Kumar <b.neerajkumar9666@gmail.com>
2021-04-05 03:15:55 +0530

    changes to post review

--- scripts/redirectsubmit.js
+++ scripts/redirectsubmit.js
@@ -1,8 +1,5 @@
-<<<<<<< HEAD
-=======
 
 
->>>>>>> 6be944df8a4b77521eb2dd5cbf019b7ddcbede54
 function redirect() {
     var url = window.location.search;
     if (url == "?location") {
@@ -10,10 +7,6 @@ function redirect() {
             window.location.href = "main.html";
         }, 1000)
     } else if (url == "?review") {
-<<<<<<< HEAD
-=======
-
->>>>>>> 6be944df8a4b77521eb2dd5cbf019b7ddcbede54
         setTimeout(function() {
             window.location.href = "location.html?";
         }, 1000)
