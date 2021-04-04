268ada55462685c1a1733547b20933cb4724a605
Richard Mac <rmac2@my.bcit.ca>
2021-04-03 22:36:53 -0700

    profile changes

--- scripts/profile.js
+++ scripts/profile.js
@@ -67,7 +67,57 @@ function getReview() {
 }
 getreview();
 
+function uploadUserProfilePic() {
+    
 
+    firebase.auth().onAuthStateChanged(function (users) {
+        var fileInput = document.getElementById("mypic-input");   
+        const image = document.getElementById("mypic-goes-here"); 
+
+        
+        fileInput.addEventListener('change', function (e) {
+            var file = e.target.files[0];
+            var blob = URL.createObjectURL(file);
+            image.src = blob;           
+
+            
+            var storageRef = storage.ref("images/" + users.uid + ".jpg"); 
+            
+            
+            storageRef.put(file) 
+                .then(function(){
+                    console.log('Uploaded to Cloud Storage.');
+                })
+
+						
+            storageRef.getDownloadURL()
+                .then(function (url) {   
+                    console.log(url);    
+                    db.collection("users").doc(users.uid).update({
+                        "profilePic": url
+                    })
+                    .then(function(){
+                        console.log('Added Profile Pic URL to Firestore.');
+                    })
+                })
+        })
+    })
+}
+
+
+
+function displayUserProfilePic() {
+    console.log("hi");
+    firebase.auth().onAuthStateChanged(function (users) {     
+        db.collection("users").doc(users.uid)                 
+            .get()                                           
+            .then(function (doc) {
+                var picUrl = doc.data().profilePic;         
+
+								$("#mypic-goes-here").attr("src", picUrl);
+            })
+    })
+}
 
 
 
