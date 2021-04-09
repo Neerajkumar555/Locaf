87b56de3bdf9b0678482bd02a46912b36ce7d8b7
Oscar La <oscarla5747@gmail.com>
2021-04-08 17:14:35 -0700

    added comments

--- scripts/redirectsubmit.js
+++ scripts/redirectsubmit.js
@@ -1,14 +1,25 @@
+
+// simply redirects the user to another page based on what page they came from
 function redirect() {
     var url = window.location.search;
+
+    // if the user came from the addlocation page
     if (url == "?location") {
+
+        // small time-out period to let the reader know their action was submitted
         setTimeout(function() {
+
+            // redirects to main page
             window.location.href = "main.html";
         }, 1000)
+
+    // if the user came from the submitreview page
     } else if (url == "?review") {
         setTimeout(function() {
+
+            // redirects to previous location page
             window.location.href = "location.html?";
         }, 1000)
     }
-
 }
 redirect();
