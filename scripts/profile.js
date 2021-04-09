6c7a9d775936863c9fcfd75246770f6edaea06e2
Oscar La <oscarla5747@gmail.com>
2021-04-08 17:11:58 -0700

    consolidated a bunch of similar functions; added readable comments

--- scripts/profile.js
+++ scripts/profile.js
@@ -1,147 +1,155 @@
-function getUsername() {
+// grabs and displays all user info
+function getUserInfo() {
     firebase.auth().onAuthStateChanged(function (users) {
         if (users) {
-            // User is signed in.
-            
-            console.log(users.uid);
+            //console.log(users.uid);
             db.collection("users").doc(users.uid)
                 .get()
                 .then(function (doc) {
-                    var n = doc.data().name;
-                    console.log(n);
-										
-                    $("#username").text(n);
-										
-                    //document.getElementById("username").innerText = n;
+                    // grabs data from user doc
+                    var name = doc.data().name;
+                    var desc = doc.data().description;
+                    var mail = doc.data().email;
+                    var numb = doc.data().number;
+                    var add = doc.data().address;
+                    var nick = doc.data().nickname;
+                    
+                    // displays grabbed data onto page
+                    $("#username").text(name);
+                    $("#desciption").text(desc);
+                    $("#email").text(mail);
+                    $("#number").text(numb);
+                    $("#address").text(add);
+                    $("#nickname").text(nick);
                 })
         } else {
             // No user is signed in.
         }
     });
 }
-getUsername();
+getUserInfo();
 
-function getEmail() {
-    firebase.auth().onAuthStateChanged(function (users) {
-        if (users) {
-            // User is signed in.
+// function getEmail() {
+//     firebase.auth().onAuthStateChanged(function (users) {
+//         if (users) {
             
-            console.log(users.uid);
-            db.collection("users").doc(users.uid)
-                .get()
-                .then(function (doc) {
-                    var n = doc.data().email;
-                    console.log(n);
+//             console.log(users.uid);
+//             db.collection("users").doc(users.uid)
+//                 .get()
+//                 .then(function (doc) {
+//                     var n = doc.data().email;
+//                     console.log(n);
 										
-                    $("#email").text(n);
+//                     // displays the e-mail of the user on their profile
+//                     $("#email").text(n);
 										
                     
-                })
-        } else {
-            // No user is signed in.
-        }
-    });
-}
-getEmail();
+//                 })
+//         } else {
+//             // No user is signed in.
+//         }
+//     });
+// }
+// getEmail();
 
-function getNumber() {
-    firebase.auth().onAuthStateChanged(function (users) {
-        if (users) {
-            // User is signed in.
+// function getNumber() {
+//     firebase.auth().onAuthStateChanged(function (users) {
+//         if (users) {
+//             // User is signed in.
             
-            console.log(users.uid);
-            db.collection("users").doc(users.uid)
-                .get()
-                .then(function (doc) {
-                    var n = doc.data().number;
-                    console.log(n);
+//             console.log(users.uid);
+//             db.collection("users").doc(users.uid)
+//                 .get()
+//                 .then(function (doc) {
+//                     var n = doc.data().number;
+//                     console.log(n);
 										
-                    $("#number").text(n);
+//                     $("#number").text(n);
 										
                     
-                })
-        } else {
-            // No user is signed in.
-        }
-    });
-}
-getNumber();
+//                 })
+//         } else {
+//             // No user is signed in.
+//         }
+//     });
+// }
+// getNumber();
 
-function getdesc() {
-    firebase.auth().onAuthStateChanged(function (users) {
-        if (users) {
-            // User is signed in.
+// function getdesc() {
+//     firebase.auth().onAuthStateChanged(function (users) {
+//         if (users) {
+//             // User is signed in.
             
-            console.log(users.uid);
-            db.collection("users").doc(users.uid)
-                .get()
-                .then(function (doc) {
-                    var n = doc.data().description;
-                    console.log(n);
+//             console.log(users.uid);
+//             db.collection("users").doc(users.uid)
+//                 .get()
+//                 .then(function (doc) {
+//                     var n = doc.data().description;
+//                     console.log(n);
 										
-                    $("#desciption").text(n);
+//                     $("#desciption").text(n);
 										
                     
-                })
-        } else {
-            // No user is signed in.
-        }
-    });
-}
-getdesc();
+//                 })
+//         } else {
+//             // No user is signed in.
+//         }
+//     });
+// }
+// getdesc();
 
-function getaddress() {
-    firebase.auth().onAuthStateChanged(function (users) {
-        if (users) {
-            // User is signed in.
+// function getaddress() {
+//     firebase.auth().onAuthStateChanged(function (users) {
+//         if (users) {
+//             // User is signed in.
             
-            console.log(users.uid);
-            db.collection("users").doc(users.uid)
-                .get()
-                .then(function (doc) {
-                    var n = doc.data().address;
-                    console.log(n);
+//             console.log(users.uid);
+//             db.collection("users").doc(users.uid)
+//                 .get()
+//                 .then(function (doc) {
+//                     var n = doc.data().address;
+//                     console.log(n);
 										
-                    $("#address").text(n);
+//                     $("#address").text(n);
 										
                     
-                })
-        } else {
-            // No user is signed in.
-        }
-    });
-}
-getaddress();
+//                 })
+//         } else {
+//             // No user is signed in.
+//         }
+//     });
+// }
+// getaddress();
 
 
 
-function getnickname() {
-    firebase.auth().onAuthStateChanged(function (users) {
-        if (users) {
-            // User is signed in.
+// function getnickname() {
+//     firebase.auth().onAuthStateChanged(function (users) {
+//         if (users) {
+//             // User is signed in.
             
-            console.log(users.uid);
-            db.collection("users").doc(users.uid)
-                .get()
-                .then(function (doc) {
-                    var n = doc.data().nickname;
-                    console.log(n);
+//             console.log(users.uid);
+//             db.collection("users").doc(users.uid)
+//                 .get()
+//                 .then(function (doc) {
+//                     var n = doc.data().nickname;
+//                     console.log(n);
 										
-                    $("#nickname").text(n);
+//                     $("#nickname").text(n);
 										
                     
-                })
-        } else {
-            // No user is signed in.
-        }
-    });
-}
+//                 })
+//         } else {
+//             // No user is signed in.
+//         }
+//     });
+// }
 
-getnickname();
+// getnickname();
 
+// uploads the user profile picture into the database and connects to their account
 function uploadUserProfilePic() {
     
-
     firebase.auth().onAuthStateChanged(function (users) {
         var fileInput = document.getElementById("mypic-input");   
         const image = document.getElementById("mypic-goes-here"); 
@@ -152,16 +160,15 @@ function uploadUserProfilePic() {
             var blob = URL.createObjectURL(file);
             image.src = blob;           
 
-            
             var storageRef = storage.ref("images/" + users.uid + ".jpg"); 
             
-            
+            // puts the file into the firestore database
             storageRef.put(file) 
                 .then(function(){
                     console.log('Uploaded to Cloud Storage.');
                 })
 
-						
+			// gets the image's firestore url and assigns it to the user
             storageRef.getDownloadURL()
                 .then(function (url) {   
                     console.log(url);    
@@ -175,26 +182,29 @@ function uploadUserProfilePic() {
         })
     })
 }
+uploadUserProfilePic();
 
 
-
+// displays the user profile picture onto the page
 function displayUserProfilePic() {
-    console.log("hi");
     firebase.auth().onAuthStateChanged(function (users) {     
+
+        // grabs the photo from the user's document
         db.collection("users").doc(users.uid)                 
             .get()                                           
             .then(function (doc) {
                 var picUrl = doc.data().profilePic;         
 
-								$("#mypic-goes-here").attr("src", picUrl);
+                // places the grabbed image into the document
+				$("#mypic-goes-here").attr("src", picUrl);
             })
     })
 }
+displayUserProfilePic();
 
 
-// Function to display reviews .....
-
-function loadreviews() {
+// grabs the reviews associated with the user from the database and loads them onto the page
+function loadReviews() {
 
     firebase.auth().onAuthStateChanged(function (user) {   
 
@@ -203,6 +213,7 @@ function loadreviews() {
         .then(function (snap) {
             $("#review-content").append('<div class="row card" id="reviewgrid"></div>');  
 
+            // places each review in a stylized card
             snap.forEach(function (doc) {
                 var name = doc.data().postedby;
                 var rating = doc.data().reviewrating;
@@ -218,5 +229,4 @@ function loadreviews() {
         })
     })
 }
-
-loadreviews();
+loadReviews();
